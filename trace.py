a76eba463c54922839d7d663f2e83fa874e8dc92
TheodorJ <teddyjohansson@live.com>
2020-01-13 17:36:08 -0500

    Implemented basic gesture similarity score

--- trace.py
+++ trace.py
@@ -8,6 +8,9 @@ from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 
 smooth_filter = np.array([[0.05, 0.05, 0.05]] * 20)
 
+triangle_files = ["data/tri1.log", "data/tri2.log", "data/tri3.log", "data/tri4.log", "data/tri5.log"]
+square_files = ["data/square1.log", "data/square2.log", "data/square3.log", "data/square4.log", "data/square5.log"]
+
 # numpy doesn't do 2d convolution :(
 def convolve2d(x, filter):
     out = []
@@ -74,6 +77,24 @@ def delta_angles(clusters):
 
     return cosines
 
+# Given two series of delta angles, return a metric for how similar they are.
+# BASIC: For each angle in x, find its closest match in y, and sum the difference.
+#        return the inverse.
+def gesture_similarity(x, y):
+    sum_differences = 0.0
+    for angle in x:
+        differences = [angle - l for l in y]
+        if differences == []:
+            break
+
+        min_diff = min(differences)
+        idx_min_diff = differences.index(min_diff)
+        y = y[idx_min_diff+1:]
+        sum_differences += min_diff **2
+
+    sum_differences = (len(x) - len(y))
+    return sum_differences
+
 if (len(sys.argv) != 2):
     print("Usage: " + sys.argv[0] + " TRACE.csv")
     exit(1)
@@ -81,18 +102,65 @@ if (len(sys.argv) != 2):
 file_name = sys.argv[1]
 gravity = get_gravity(load_trace("data/rest.csv"))
 
+triangle_data =[]
+for fd in triangle_files:
+    trace = load_trace(fd)
+    trace = remove_gravity(trace, gravity)
+    trace = convolve2d(trace, smooth_filter)
+    clusters, cluster_sizes = k_cluster(trace)
+    assert(len(clusters) == len(cluster_sizes))
+
+
+    print(len(clusters))
+    triangle_data.append(delta_angles(clusters))
+
+square_data =[]
+for fd in square_files:
+    trace = load_trace(fd)
+    trace = remove_gravity(trace, gravity)
+    trace = convolve2d(trace, smooth_filter)
+    clusters, cluster_sizes = k_cluster(trace)
+    assert(len(clusters) == len(cluster_sizes))
+
+    square_data.append(delta_angles(clusters))
+
+
+
 trace = load_trace(file_name)
 trace = remove_gravity(trace, gravity)
 trace = convolve2d(trace, smooth_filter)
 clusters, cluster_sizes = k_cluster(trace)
-assert(len(clusters) == len(cluster_sizes))
+"""assert(len(clusters) == len(cluster_sizes))
 
 print("")
 print(trace.shape)
 print(len(clusters))
 print(clusters)
-print(delta_angles(clusters))
+print(delta_angles(clusters))"""
 
+print("")
+print(len(clusters))
+print("Similarity to square data:")
+square_similarities = []
+for sq in square_data:
+    square_similarities.append(gesture_similarity(delta_angles(clusters), sq))
+    print(gesture_similarity(delta_angles(clusters), sq))
+
+print("Max square similarity:")
+print(max(square_similarities))
+
+print("Similarity to triangle data:")
+triangle_similarities = []
+for tr in triangle_data:
+    triangle_similarities.append(gesture_similarity(delta_angles(clusters), tr))
+    print(gesture_similarity(delta_angles(clusters), tr))
+
+print("Max triangle similarity")
+print(max(triangle_similarities))
+
+
+
+exit(1)
 # Plot trace
 
 # Set up 3D plot
