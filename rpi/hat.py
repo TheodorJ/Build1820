415bed9517e1741672072fbc8fec9dc4776bd3d4
Maxwell Johnson <maximusjohnsonus@gmail.com>
2020-01-16 23:13:48 -0500

    Added wifi to hat

--- rpi/hat.py
+++ rpi/hat.py
@@ -1,13 +1,21 @@
-import pygatt
+import telnetlib
 from binascii import hexlify
-
 import time
+
 def now():
     return int(round(time.time() * 1000))
 
-adapter = pygatt.GATTToolBackend()
-addr = "a4:cf:12:77:11:1a" # The one not in the wand
-# addr = "a4:cf:12:77:20:ca" # The one in the wand
+"""
+Network commands
+all of which are single ASCII charactes
+
+U/D/L/R - wand direction
+V/^     - wand button down/up
+B/E     - game begin/end
+
+"""
+
+spells = ["U", "D", "L", "R"]
 
 num_wands_ready = 0
 state = "GAME_END"
@@ -16,6 +24,11 @@ player_health = {}
 player_last_hor_defend = {}
 player_last_ver_defend = {}
 defend_duration = 1000
+
+# IPs of the wands
+ips = ["192.168.4.11","192.168.4.12"]
+# telnet handles
+tns = {}
 
 def other_player(handle):
     return [x for x in player_health.keys() if x != handle][0]
@@ -27,7 +40,7 @@ def player_is_ver_defended(player):
     return (now() - player_last_ver_defend[player]) > defend_duration
 
 
-def handle_data(handle, value):
+def process_message(ip, value):
     """
     handle -- integer, characteristic read handle the data was received on
     value -- bytearray, the data returned in the notification
@@ -39,20 +52,19 @@ def handle_data(handle, value):
         player_last_hor_defend[handle] = now()
         player_last_ver_defend[handle] = now()
 
-    if(value == "BTN_DOWN"):
+    if(value == "V"): # button down
         if state == "GAME_END":
             num_wands_ready += 1
         if(num_wands_ready == 2):
             state = "GAME_START"
 
-    if(value == "BTN_UP"):
+    if(value == "^"): # button up
         if state == "GAME_END":
             num_wands_ready -= 1
 
-    if(value[:6] == "CAST "):
+    if(value in spells): # cast
         if(state == "GAME_START"):
-            spell = value[6]
-            if(spell == 0):   # LEFT
+            if(value == "L"):   # LEFT
                 # If the other player isn't defended, subtract a health point
                 other_p = other_player(handle)
 
@@ -63,11 +75,11 @@ def handle_data(handle, value):
                     # GAME END
                     state = "GAME_END"
 
-            elif(spell == 1): # RIGHT
+            elif(value == "R"): # RIGHT
                 player_last_hor_defend[handle] = now()
-            elif(spell == 2): # UP
+            elif(value == "U"): # UP
                 player_last_ver_defend[handle] = now()
-            elif(spell == 3): # DOWN
+            elif(value == "D"): # DOWN
                 # If the other player isn't defended, subtract a health point
                 other_p = other_player(handle)
 
@@ -81,14 +93,25 @@ def handle_data(handle, value):
                 pass
 
 
-try:
-    adapter.start()
-    device = adapter.connect(addr) #"a4:cf:12:77:20:ca")
-    print("connected")
+for ip in ips:
+    tns[ip] = telnetlib.Telnet(ip)
+    print("Connected to " + ip)
 
-    device.subscribe("beb5483e-36e1-4688-b7f5-ea07361b26a8",
-                     callback=handle_data)
-    while(1):
-        pass
-finally:
-    adapter.stop()
+while 1:
+    # Main game loop
+
+    for ip in ips:
+        tn = tns[ip]
+        try:
+            spell = tn.read_eager()
+
+            # read_eager returns "" if no data available
+            if (spell != ""):
+                print("received " + spell + " on " + ip)
+
+                # Process each letter in the message
+                for c in spell.decode("UTF-8"):
+                    process_message(ip, c)
+
+        except EOFError:
+            print(ip + " disconnected")
